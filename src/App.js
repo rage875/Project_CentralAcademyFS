52d80118e3a8ec0daff360d0ff78ab4a51ccaa89
Luis03 <rage.875@gmail.com>
2019-07-01 19:18:08 -0500

    Function example within app.js

--- src/App.js
+++ src/App.js
@@ -2,24 +2,36 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function displayLogo(){
+  return(
+    <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>Edit <code>src/App.js</code> and save to reload.</p>
+      <a className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer">Learn React
+      </a>
+    </header>
+  </div>
+  )
+}
+
+function createNavBar(){
+  return(
+    <div className = "Nav">
+      <ul>
+        <li><a href="/">Logo</a></li>
+        <li><a href="/">Printer's profile</a></li>
+      </ul> 
+    </div>
+  )
+}
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    createNavBar()
   );
 }
 
