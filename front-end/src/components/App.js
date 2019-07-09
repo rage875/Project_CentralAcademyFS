854ae1b82fabd745123d97c8e4b13659a0d87bb4
Luis03 <rage.875@gmail.com>
2019-07-09 17:11:25 -0500

    Front:App: folder structure and icon update

---
+++ front-end/src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../styles/App.css';
+
+import Header from './Header.js';
+
+function App() {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+}
+
+export default App;
