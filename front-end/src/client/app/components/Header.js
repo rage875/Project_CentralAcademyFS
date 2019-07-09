45f58f7c55d1918a3b4c92e5aaac5d253fadec90
Luis03 <rage.875@gmail.com>
2019-07-09 12:41:34 -0500

    Header: Account icon dropdown list updated

--- front-end/src/client/app/components/Header.js
+++ front-end/src/client/app/components/Header.js
@@ -5,36 +5,41 @@ import NavDropdown from 'react-bootstrap/NavDropdown';
 import FormControl from 'react-bootstrap/FormControl';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import DropdownButton from 'react-bootstrap/DropdownButton';
 
 import '../styles/Header.css';
 
+import dropDownIcon from '../pics/iconic/person.svg';
+
 class Header extends React.Component {
   render() {
+    const navDropdownIcon = 
+    (<img src= {dropDownIcon} className="dropDownIcon" alt="Icon" />);
+
     return (
-      <Navbar bg="dark" expand="lg">
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mr-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-          <Form inline>
-            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-            <Button variant="outline-success">Search</Button>
-          </Form>
-          <Nav inline>
-            <Nav.Link href="#home/register"> Register</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
+    <Navbar bg="dark" expand="sm" variant="dark">
+      <Navbar.Brand href="/">3D PrinNet</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#link">Link</Nav.Link>
+        </Nav>
+        <Form inline>
+          <FormControl type="text" placeholder="Search" className="mr-sm-1" size="sm" />
+          <Button variant="outline-info" size="sm">Search</Button>
+        </Form>
+        <DropdownButton alignRight size="sm" variant="dark" className="ml-sm-1"
+          title={navDropdownIcon}
+          id="dropdown-button-drop-down"
+        >
+          <NavDropdown.Item href="#join">Sign up</NavDropdown.Item>
+          <NavDropdown.Item href="#login">Sign in</NavDropdown.Item>
+          <NavDropdown.Divider />
+          <NavDropdown.Item href="#help">Help</NavDropdown.Item>
+        </DropdownButton>
+      </Navbar.Collapse>
+    </Navbar>
     );
   }
 }
